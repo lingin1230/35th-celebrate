390b266ce56da84495de1b91efecd158b5e6237c
Gin <gin@fugu.ooo>
2022-07-06 11:51:34 +0800

    feat(foreword): build foreword

--- src/pages/Foreword.jsx
+++ src/pages/Foreword.jsx
@@ -1,5 +1,14 @@
 export default function Foreword() {
     return (
-        'Foreword'
+        <div className="foreword">
+            <div className="content">
+                <div className="paper">
+                    <div className="text">
+                        Dear Furecool: <br/>
+                        123123123123123123
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
