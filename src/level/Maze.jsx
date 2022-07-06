17d82bb103a712556a285f66066ffab7eff6beac
Gin <gin@fugu.ooo>
2022-07-06 23:03:43 +0800

    feat(monster game): add playing function

--- src/level/Maze.jsx
+++ src/level/Maze.jsx
@@ -1,7 +1,7 @@
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { PerspectiveCamera, OrbitControls, FirstPersonControls } from '@react-three/drei'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import { Canvas, ambientLight, pointLight } from "@react-three/fiber"
 import { Suspense } from "react"
@@ -18,20 +18,25 @@ import dungeon from 'url:../assets/gltf/dungeon.glb'
 import meatSteak from 'url:../assets/gltf/meat-steak.glb'
 import fantasyBook from 'url:../assets/gltf/fantasy-book.glb'
 
+import dungeonMusic from 'url:../assets/sound/dungeon.wav'
+
 export default function Maze(props) {
 
-    // const setLoaded = props.setLoaded
-    const gltf = useLoader(GLTFLoader, dungeon)
-    
-    // useEffect(() => {
-    //     setLoaded(true)
-    // }, [])
-
+    const setStatus = props.setStatus
+    const bgSound = useRef()
+    const maze = useLoader(GLTFLoader, dungeon)
+    const present = useLoader(GLTFLoader, ramen)
 
 
     const [ hover, setHover ] = useState()
     const [ color, setColor ] = useState()
 
+
+    useEffect(() => {
+        bgSound.current.src = dungeonMusic
+        bgSound.current.loop = true
+        bgSound.current.play()
+    }, [])
 
     useEffect(() => {
         console.log('hover', hover)
@@ -40,16 +45,19 @@ export default function Maze(props) {
 
     return(
         <>
+        <audio ref={bgSound} />
         <Canvas>
             <ambientLight />
-            <pointLight position={[10, 10, 10]} />
+            {/* <pointLight position={[10, 10, 10]} /> */}
             {/* <primitive object={gltf.scene} scale={3} position={[0, -2, 0]}/> */}
 
             {/* Dungeon position setting */}
-            <primitive object={gltf.scene} scale={3} position={[19, -2, -180]}/>
-            <FirstPersonControls lookSpeed={0.1} lookVertical={false} constrainVertical={false} movementSpeed={8}/>
+            {/* <primitive object={gltf.scene} scale={3} position={[19, -2, -180]}/> */}
+            {/* <FirstPersonControls lookSpeed={0.1} lookVertical={false} constrainVertical={false} movementSpeed={8}/> */}
 
-            {/* <OrbitControls /> */}
+            <primitive object={maze.scene} scale={3} position={[19, -2, -180]}/>
+            <primitive object={present.scene} scale={10} position={[19, -2, -180]}/>
+            <OrbitControls />
             {/* <OrthographicCamera position={[0, 0, 100]} zoom={30} /> */}
         </Canvas>
         </>
--- src/level/Monster.jsx
+++ src/level/Monster.jsx
@@ -1,5 +1,65 @@
+import classNames from 'classnames'
+import axios from 'axios'
+import { useEffect, useState, useRef } from 'react'
+import jungleMusic from 'url:../assets/sound/jungle.wav'
+
 export default function Monster() {
+
+    const [ monkeyBlood, setMonkeyBlood ] = useState(100)
+    const [ girlBlood, setGirlBlood ] = useState(100)
+    const [ talk, setTalk ] = useState(null)
+    const bgSound = useRef()
+
+    useEffect(() => {
+        bgSound.current.src = jungleMusic
+        bgSound.current.loop = true
+        bgSound.current.play()
+    }, [])
+
+    useEffect(() => {
+        if (monkeyBlood === 0) {
+            setTalk('monkey')
+            setTimeout(() => {
+                document.location.href = '/lobby'
+            }, [5000])
+        }
+        if (girlBlood === 0) {
+            setTalk('girl')
+            axios.patch('http://localhost:3000/level_status', {
+                "monster": 'complete',
+                "maze": 'unlock'
+            })
+            setTimeout(() => {
+                document.location.href = '/lobby'
+            }, [5000])
+        }
+    }, [monkeyBlood, girlBlood])
+
+
+    const statusClass = classNames('status', {
+        'success': girlBlood === 0,
+        'fail': monkeyBlood === 0
+    })
+    const monkeyClass = classNames('monkey', 'blood', `blood-${monkeyBlood >=0 ? monkeyBlood : 0}`, {
+        'talk': talk === 'monkey'
+    })
+    const girlClass = classNames('girl', 'blood', `blood-${girlBlood >=0 ? girlBlood : 0} `, {
+        'talk': talk === 'girl'
+    })
+
     return (
-        'monster'
+        <div className="monster">
+            <audio ref={bgSound} />
+            <div className={statusClass}></div>
+            <div className={monkeyClass} onClick={() => {monkeyBlood > 0 && setMonkeyBlood(monkeyBlood - 10)}}>
+                <div className="monkey-talk">我...我是 QQ 啊！你怎麼把我殺了！！</div>
+            </div>
+            <div className={girlClass} onClick={() => {girlBlood > 0 && setGirlBlood(girlBlood - 10)}}>
+                <div className="girl-talk">你！你怎麼知道我是邪惡女童！<br/>ㄅㄜ ㄌㄜ...</div>
+            </div>
+            <div className="content">
+
+            </div>
+        </div>
     )
 }
